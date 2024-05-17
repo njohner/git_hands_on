21275ecc687b15b8b3f8321f6ea3a3770b41c3b3
Niklaus Johner <niklaus.johner@a3.epfl.ch>
2024-05-17 11:20:28 +0200

    Add highlighting to script to make code fragments.

--- scripts/make_code_fragments.py
+++ scripts/make_code_fragments.py
@@ -3,22 +3,20 @@ This is a simple helper script to prepare fragments for highlighting
 from code blocks.
 """
 block = """
-git status
-git apply patches/modify_files_and_add_new_file.md
-git status
-git diff
-git add training/file*.md
-git diff
-git diff --staged
-git commit -m "Modified file1 and file2 and added file3.md"
-git log -n2 --oneline
+git log -n5 --oneline --graph --decorate --all
+git rebase temp
+git log -n5 --oneline --graph --decorate --all
+git checkout temp
+git merge dev
+git log -n5 --oneline --graph --decorate --all
+git branch -d dev
 """
 
 fragment = """
-::: {{.fragment .current-only data-code-focus="{}"}}
+::: {{.fragment .current-only data-code-focus="{i}" fragment-index={i}}}
 ```{{bash}}
 #| echo: false
-{}
+{code}
 ```
 :::"""
 
@@ -26,4 +24,4 @@ counter = 0
 for line in block.split("\n"):
     if line.strip():
         counter += 1
-        print(fragment.format(counter, line))
+        print(fragment.format(i=counter, code=line))
